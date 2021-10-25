d997c1a47019128d516722acdb4cd65eee30e2ad
Phiwe Pohleli <pohleliphiwe@gmail.com>
2021-10-25 15:08:01 +0200

    first commit

---
+++ Profiles/AuthorProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CourseLibrary.API.Helpers;
+
+namespace CourseLibrary.API.Profiles
+{
+  public class AuthorProfile : Profile
+  {
+    public AuthorProfile()
+    {
+      CreateMap<Entities.Author, Models.AuthorDto>()
+        .ForMember(
+          dest => dest.Name,
+          opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+        .ForMember(
+          dest => dest.Age,
+          opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge()));
+      CreateMap<Models.AuthorForCreationDto, Entities.Author>();
+    }
+  }
+}
---
+++ Profiles/CoursesProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace CourseLibrary.API.Profiles
+{
+  public class CoursesProfile : Profile
+  {
+    public CoursesProfile()
+    {
+      CreateMap<Entities.Course, Models.CourseDto>();
+      CreateMap<Models.CourseForCreationDto, Entities.Course>();
+      CreateMap<Models.CourseForUpdateDto, Entities.Course>();
+      CreateMap<Entities.Course, Models.CourseForUpdateDto>();
+    }
+  }
+}
